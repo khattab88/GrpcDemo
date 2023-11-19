4624b18966d015accaa6db5c9ad3dc33a48a79b5
khattab88 <m_khatab_88@hotmail.com>
2023-11-19 17:33:24 +0200

    consume custome grpc service

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,7 +7,7 @@ namespace Client
     {
         static async Task Main(string[] args)
         {
-            #region Basic Service
+            #region Grreet Service
 
             //var channel = GrpcChannel.ForAddress("https://localhost:5001");
             //var client = new Greeter.GreeterClient(channel);
@@ -20,7 +20,19 @@ namespace Client
             #endregion
 
 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
 
+            Console.Out.WriteLine("Enter Customer Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            var request = new CustomerLookupModel() { UserId = id };
+            var response = await client.GetCustomerInfoAsync(request);
+
+            Console.Out.WriteLine($"Customer# {response.Id}: {response.FirstName} {response.LastName}");
+
+
+            Console.Out.WriteLine("Press any key to exit");
             Console.ReadLine();
         }
     }
