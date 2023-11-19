c166812cff22c54d1ed7aa89e56fa2f593f08f17
khattab88 <m_khatab_88@hotmail.com>
2023-11-19 16:24:04 +0200

    create grpc client

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using Server;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var request = new HelloRequest { Name = "gRPC Demo" };
+            var response = await client.SayHelloAsync(request);
+
+            Console.Out.WriteLine(response.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
